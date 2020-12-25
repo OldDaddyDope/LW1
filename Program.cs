a72bcf051cf54540c68fecc7b1b9abc5971eddb1
OldDaddyDope <71905747+OldDaddyDope@users.noreply.github.com>
2020-12-25 15:47:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите x:");
+                uint x = uint.Parse(Console.ReadLine());
+                if (x > 3 || x < -1) Console.WriteLine("выражение истинно");
+                else Console.WriteLine("выражение неистенно");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
