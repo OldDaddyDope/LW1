ee80f8e394f1ef6f551ce30ec990a1fb829c8f69
OldDaddyDope <71905747+OldDaddyDope@users.noreply.github.com>
2020-12-25 15:52:26 +0200

    Add files via upload

---
+++ 2.2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите текущий час:");
+                uint x = uint.Parse(Console.ReadLine());
+                if (x >= 0 && x <= 12) Console.WriteLine(x + "am");
+                 if (x >= 13 && x <= 24) Console.WriteLine(x-12 + "pm");                   
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
