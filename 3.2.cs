c9eec7b7697a8d61ade04fac53204ff5ccd664a1
OldDaddyDope <71905747+OldDaddyDope@users.noreply.github.com>
2020-12-25 15:57:04 +0200

    Add files via upload

---
+++ 3.2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace proizvedenie_kubov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] Arr = { 1, 2, 3 };
+            int Pow = 3;
+
+            double Multiply = 1;
+
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                Console.WriteLine($"Куб числа {Arr[i]} = {Math.Pow(Arr[i], Pow)}");
+                Multiply *= Math.Pow(Arr[i], Pow);
+            }
+            Console.WriteLine($"Произведение кубов чисел = {Multiply}");
+        }
+    }
+}
